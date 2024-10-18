568ad818a9688eae9e12f6c2b9a37974733d1ee7
Blu Corbel <blucorbel29@gmail.com>
2024-10-18 16:37:10 +0100

    Update Cron-jobs.d

--- Cron-jobs.d
+++ Cron-jobs.d
@@ -3,3 +3,9 @@
 
 # Run Nmap scan every 30 minutes
 */30 * * * * /path/to/nmap_scan.sh
+
+# Run social media harvesting daily
+0 1 * * * /path/to/social_media_harvest.sh
+
+# Monitor Telegram messages every 30 minutes
+*/30 * * * * /path/to/telegram_monitor.py
